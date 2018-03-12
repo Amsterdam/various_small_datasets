b55bb5400cad5936c19c395d9b060683a293baf8
Bart Heupers <bart.heupers@gmail.com>
2018-03-12 17:32:45 +0100

    Rename static URL

--- src/various_small_datasets/settings.py
+++ src/various_small_datasets/settings.py
@@ -74,7 +74,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_URL = '/various_small_datasets/static/'
+STATIC_URL = '/vsd/static/'
 STATIC_ROOT = '/static/'
 
 # SWAGGER
