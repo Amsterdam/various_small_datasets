95ba9e676884c4d345b79bca38a1321065210b27
Bart Heupers <bart.heupers@gmail.com>
2018-03-13 14:25:14 +0100

    Fix import step 2

--- src/biz/import/import.sh
+++ src/biz/import/import.sh
@@ -13,11 +13,10 @@ DATABASE_USER=${DATABASE_USER:-various_small_datasets}
 DATABASE_PASSWORD=${DATABASE_PASSWORD:-insecure}
 
 # Register database credentials
+PGPASSFILE=~/.pgpass_various_small_datasets
 CREDENTIALS="${DATABASE_HOST}:${DATABASE_PORT}:${DATABASE_NAME}:${DATABASE_USER}:${DATABASE_PASSWORD}"
-if [ ! -f ~/.pgpass -o ! grep -Fxq $CREDENTIALS ~/.pgpass > /dev/null ]; then
-    echo $CREDENTIALS >> ~/.pgpass
-    chmod 600 ~/.pgpass
-fi
+echo $CREDENTIALS > ${PGPASSFILE}
+chmod 600 ${PGPASSFILE}
 
 psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME} -c "drop table if exists biz_data;"
 cat biz_data_create.sql| psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME}
