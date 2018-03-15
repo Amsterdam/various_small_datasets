47f986b6d673548328e2083f651b9e8222df4898
Bart Heupers <bart.heupers@gmail.com>
2018-03-15 16:43:50 +0100

    DP-5371 - Use /tmp dir for writing tmp data

--- src/trm/import/import.sh
+++ src/trm/import/import.sh
@@ -21,43 +21,45 @@ CREDENTIALS="${DATABASE_HOST}:${DATABASE_PORT}:${DATABASE_NAME}:${DATABASE_USER}
 echo $CREDENTIALS > ${PGPASSFILE}
 chmod 600 ${PGPASSFILE}
 
+TMPDIR=/tmp/trm
+
 # Move to trm directory
 cd ..
-mkdir -p tmp
-unzip data/Tram\ KGEs.zip -d tmp
-unzip data/Metro\ KGEs.zip -d tmp
+mkdir -p ${TMPDIR}
+unzip data/Tram\ KGEs.zip -d ${TMPDIR}
+unzip data/Metro\ KGEs.zip -d ${TMPDIR}
 
 # Convert Shape files to PostGIS SQL dump file
-ogr2ogr -f "PGDump" tmp/trm_tram.sql tmp/KGE_hartlijnen_Amsterdam_2.050.shp
-ogr2ogr -f "PGDump" tmp/trm_metro.sql tmp/Metro\ KGEs.shp
+ogr2ogr -f "PGDump" ${TMPDIR}/trm_tram.sql ${TMPDIR}/KGE_hartlijnen_Amsterdam_2.050.shp
+ogr2ogr -f "PGDump" ${TMPDIR}/trm_metro.sql ${TMPDIR}/Metro\ KGEs.shp
 
 # Convert SQL file to utf-8 SQL file
-iconv -f iso-8859-1 -t utf-8  tmp/trm_tram.sql > tmp/trm_tram.utf8.sql
-iconv -f iso-8859-1 -t utf-8  tmp/trm_metro.sql > tmp/trm_metro.utf8.sql
+iconv -f iso-8859-1 -t utf-8  ${TMPDIR}/trm_tram.sql > ${TMPDIR}/trm_tram.utf8.sql
+iconv -f iso-8859-1 -t utf-8  ${TMPDIR}/trm_metro.sql > ${TMPDIR}/trm_metro.utf8.sql
 
 # Replace table names
-sed -i -- 's/KGE_hartlijnen_Amsterdam_2/public/g' tmp/trm_tram.utf8.sql
-sed -i -- 's/"050/"trm_tram/g' tmp/trm_tram.utf8.sql
-sed -i -- "s/'050/'trm_tram/g" tmp/trm_tram.utf8.sql
+sed -i -- 's/KGE_hartlijnen_Amsterdam_2/public/g' ${TMPDIR}/trm_tram.utf8.sql
+sed -i -- 's/"050/"trm_tram/g' ${TMPDIR}/trm_tram.utf8.sql
+sed -i -- "s/'050/'trm_tram/g" ${TMPDIR}/trm_tram.utf8.sql
 
-sed -i -- 's/metro kges/trm_metro/g' tmp/trm_metro.utf8.sql
+sed -i -- 's/metro kges/trm_metro/g' ${TMPDIR}/trm_metro.utf8.sql
 
 # Replace LINESTRING with GEOMETRY because the data also contains MULTILINESTRING elements
-sed -i -- "s/'LINESTRING'/'GEOMETRY'/g" tmp/trm_metro.utf8.sql
-sed -i -- "s/'LINESTRING'/'GEOMETRY'/g" tmp/trm_tram.utf8.sql
+sed -i -- "s/'LINESTRING'/'GEOMETRY'/g" ${TMPDIR}/trm_metro.utf8.sql
+sed -i -- "s/'LINESTRING'/'GEOMETRY'/g" ${TMPDIR}/trm_tram.utf8.sql
 
 # Replace column names with spaces
-sed -i -- 's/"baanvak in"/"baanvak_in"/g; s/"baanvak _1"/"baanvak_1"/g; s/"lijnnrs aa"/"lijnnrs_aa"/g' tmp/trm_tram.utf8.sql
+sed -i -- 's/"baanvak in"/"baanvak_in"/g; s/"baanvak _1"/"baanvak_1"/g; s/"lijnnrs aa"/"lijnnrs_aa"/g' ${TMPDIR}/trm_tram.utf8.sql
 
 # Replace HTML entities
-sed -i -- 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g;' tmp/trm_tram.utf8.sql
+sed -i -- 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g;' ${TMPDIR}/trm_tram.utf8.sql
 
-cat tmp/trm_tram.utf8.sql | psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME}
-cat tmp/trm_metro.utf8.sql | psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME}
+cat ${TMPDIR}/trm_tram.utf8.sql | psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME}
+cat ${TMPDIR}/trm_metro.utf8.sql | psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME}
 
 psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME} -c "SELECT UpdateGeometrySRID('trm_tram', 'wkb_geometry', 28992);"
 psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME} -c "SELECT UpdateGeometrySRID('trm_metro', 'wkb_geometry', 28992);"
 
-rm -Rf tmp
+rm -Rf ${TMPDIR}
 
 cd $CUR_DIR
