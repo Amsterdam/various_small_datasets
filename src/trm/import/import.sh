28f9924c5044635ae781c68bd2cb662acb589279
Bart Heupers <bart.heupers@gmail.com>
2018-04-11 09:46:13 +0200

    Update tram dataset. Make import more suitable for partial import using "_new" tables

--- src/trm/import/import.sh
+++ src/trm/import/import.sh
@@ -1,25 +1,16 @@
 #!/usr/bin/env bash
 
-# Convert Tram Metro Data to sql files and import in TRM_ tables
+# Convert Tram Metro Data to sql files and import in trm_tram_new and tram_metro_new tables
+# If all is OK the tables are renamed to trm_tram and trm_metro, and the indexes are
+# also renamed to make future imports possible
 
 set -e   # stop on any error
+set -u
 
 CUR_DIR=`pwd`
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $SCRIPT_DIR
-
-DATABASE_HOST=${DATABASE_HOST:-localhost}
-DATABASE_PORT=${DATABASE_PORT:-5408}
-DATABASE_NAME=${DATABASE_NAME:-various_small_datasets}
-DATABASE_USER=${DATABASE_USER:-various_small_datasets}
-DATABASE_PASSWORD=${DATABASE_PASSWORD:-insecure}
-
-# Register database credentials
-export PGPASSFILE=~/.pgpass_various_small_datasets
-CREDENTIALS="${DATABASE_HOST}:${DATABASE_PORT}:${DATABASE_NAME}:${DATABASE_USER}:${DATABASE_PASSWORD}"
-echo $CREDENTIALS > ${PGPASSFILE}
-chmod 600 ${PGPASSFILE}
 
 TMPDIR=/tmp/trm
 
@@ -30,31 +21,45 @@ unzip data/Tram\ KGEs.zip -d ${TMPDIR}
 unzip data/Metro\ KGEs.zip -d ${TMPDIR}
 
 # Convert Shape files to PostGIS SQL dump file
-ogr2ogr -f "PGDump" -nlt GEOMETRY ${TMPDIR}/trm_tram.sql ${TMPDIR}/KGE_hartlijnen_Amsterdam_2.050.shp
-ogr2ogr -f "PGDump" -nlt GEOMETRY ${TMPDIR}/trm_metro.sql ${TMPDIR}/Metro\ KGEs.shp
+ogr2ogr -f "PGDump" -nlt GEOMETRY -t_srs EPSG:28992 -s_srs EPSG:28992 -nln trm_tram_new ${TMPDIR}/trm_tram.sql ${TMPDIR}/KGE_hartlijnen_Amsterdam_2.054.shp
+ogr2ogr -f "PGDump" -nlt GEOMETRY -t_srs EPSG:28992 -s_srs EPSG:28992 -nln trm_metro_new ${TMPDIR}/trm_metro.sql ${TMPDIR}/Metro\ KGEs.shp
 
 # Convert SQL file to utf-8 SQL file
 iconv -f iso-8859-1 -t utf-8  ${TMPDIR}/trm_tram.sql > ${TMPDIR}/trm_tram.utf8.sql
 iconv -f iso-8859-1 -t utf-8  ${TMPDIR}/trm_metro.sql > ${TMPDIR}/trm_metro.utf8.sql
 
-# Replace table names
-sed -i -- 's/KGE_hartlijnen_Amsterdam_2/public/g' ${TMPDIR}/trm_tram.utf8.sql
-sed -i -- 's/"050/"trm_tram/g' ${TMPDIR}/trm_tram.utf8.sql
-sed -i -- "s/'050/'trm_tram/g" ${TMPDIR}/trm_tram.utf8.sql
-
-sed -i -- 's/metro kges/trm_metro/g' ${TMPDIR}/trm_metro.utf8.sql
-
-# Replace column names with spaces
-sed -i -- 's/"baanvak in"/"baanvak_in"/g; s/"baanvak _1"/"baanvak_1"/g; s/"lijnnrs aa"/"lijnnrs_aa"/g' ${TMPDIR}/trm_tram.utf8.sql
+# Remove drop table statements. They give error with ON_ERROR_STOP
+perl -i -ne "print unless /DROP TABLE/" ${TMPDIR}/trm_tram.utf8.sql
+perl -i -ne "print unless /DROP TABLE/" ${TMPDIR}/trm_metro.utf8.sql
 
 # Replace HTML entities
 sed -i -- 's/&/\&amp;/g; s/</\&lt;/g; s/>/\&gt;/g;' ${TMPDIR}/trm_tram.utf8.sql
 
-cat ${TMPDIR}/trm_tram.utf8.sql | psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME}
-cat ${TMPDIR}/trm_metro.utf8.sql | psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME}
+export PGHOST=${DATABASE_HOST:-localhost}
+export PGPORT=${DATABASE_PORT:-5408}
+export PGDATABASE=${DATABASE_NAME:-various_small_datasets}
+export PGUSER=${DATABASE_USER:-various_small_datasets}
+export PGPASSWORD=${DATABASE_PASSWORD:-insecure}
 
-psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME} -c "SELECT UpdateGeometrySRID('trm_tram', 'wkb_geometry', 28992);"
-psql -h ${DATABASE_HOST} -p ${DATABASE_PORT} -U ${DATABASE_USER} ${DATABASE_NAME} -c "SELECT UpdateGeometrySRID('trm_metro', 'wkb_geometry', 28992);"
+psql -X --set ON_ERROR_STOP=on <<SQL
+BEGIN;
+DROP TABLE IF EXISTS trm_tram_new, trm_metro_new;
+COMMIT;
+\i ${TMPDIR}/trm_metro.utf8.sql
+\i ${TMPDIR}/trm_tram.utf8.sql
+BEGIN;
+ALTER TABLE IF EXISTS trm_metro RENAME TO trm_metro_old;
+ALTER TABLE trm_metro_new RENAME TO trm_metro;
+DROP TABLE IF EXISTS trm_metro_old;
+ALTER INDEX trm_metro_new_pk RENAME TO trm_metro_pk;
+ALTER INDEX trm_metro_new_wkb_geometry_geom_idx RENAME TO trm_metro_wkb_geometry_geom_idx;
+ALTER TABLE IF EXISTS trm_tram RENAME TO trm_tram_old;
+ALTER TABLE trm_tram_new RENAME TO trm_tram;
+DROP TABLE IF EXISTS trm_tram_old;
+ALTER INDEX trm_tram_new_pk RENAME TO trm_tram_pk;
+ALTER INDEX trm_tram_new_wkb_geometry_geom_idx RENAME TO trm_tram_wkb_geometry_geom_idx;
+COMMIT;
+SQL
 
 rm -Rf ${TMPDIR}
 
