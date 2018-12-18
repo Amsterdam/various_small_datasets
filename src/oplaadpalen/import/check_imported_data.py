6c64cfb7ec94464cdd25cf843dad9cb8858fc1b3
Bart Heupers <bart.heupers@gmail.com>
2018-12-18 08:42:44 +0100

    Move duplicated code to utils/check_imported_data.py

--- src/oplaadpalen/import/check_imported_data.py
+++ src/oplaadpalen/import/check_imported_data.py
@@ -1,55 +1,14 @@
 #!/usr/bin/env python
-import psycopg2
-import logging
-
-from various_small_datasets.settings import DATABASES
-
-log = logging.getLogger(__name__)
-
-database = DATABASES['default']
-dbname = database['NAME']
-user = database['USER']
-password = database['PASSWORD']
-host = database['HOST']
-port = database['PORT']
+from utils.check_imported_data import main, assert_count_minimum, assert_count_zero
 
 sql_checks = [
-    ('count', "select count(*) from oplaadpalen_new", lambda x: x[0][0] >= 1354),
+    ('count', "select count(*) from oplaadpalen_new", assert_count_minimum(1354)),
     ('geometrie', """
 select count(*) from oplaadpalen_new where
 wkb_geometry is null or ST_IsValid(wkb_geometry) = false or ST_GeometryType(wkb_geometry) <> 'ST_Point'
     """,
-     lambda x: x[0][0] == 0),
+     assert_count_zero()),
 ]
 
-
-def do_checks(conn):
-    with conn.cursor() as curs:
-        for (name, sql, check) in sql_checks:
-            print(f"check {name}")
-            curs.execute(sql)
-            result = curs.fetchall()
-            if not check(result):
-                print(f"Failed : {sql}")
-                return False
-
-    return True
-
-
-def main():
-    try:
-        dsn = f"dbname='{dbname}' user='{user}' password='{password}' host='{host}' port={port}"
-        with psycopg2.connect(dsn) as conn:
-            result = do_checks(conn)
-            if not result:
-                exit(1)
-    except psycopg2.Error as exc:
-        print("Unable to connect to the database", exc)
-        exit(1)
-
-    print("All checks passed")
-    exit(0)
-
-
 if __name__ == '__main__':
-    main()
+    main(sql_checks)
--- src/trm/import/check_imported_data.py
+++ src/trm/import/check_imported_data.py
@@ -1,32 +1,5 @@
 #!/usr/bin/env python
-import psycopg2
-import logging
-
-from various_small_datasets.settings import DATABASES
-from django.core.validators import URLValidator
-from django.core.exceptions import ValidationError
-
-log = logging.getLogger(__name__)
-
-database = DATABASES['default']
-dbname = database['NAME']
-user = database['USER']
-password = database['PASSWORD']
-host = database['HOST']
-port = database['PORT']
-
-url_validator = URLValidator()
-
-
-def all_valid_url(urls):
-    try:
-        for url in urls:
-            url_validator(url[0])
-    except ValidationError as e:
-        print(f"URL validation error for {url[0]}: ", e)
-        return False
-    return True
-
+from utils.check_imported_data import main, assert_count_minimum, assert_count_zero
 
 sql_checks = [
     ('count_metro', "select count(*) from trm_metro_new", lambda x: x[0][0] > 700),
@@ -40,33 +13,5 @@ select column_name from information_schema.columns where table_schema = 'public'
 ]
 
 
-def do_checks(conn):
-    with conn.cursor() as curs:
-        for (name, sql, check) in sql_checks:
-            print(f"check {name}")
-            curs.execute(sql)
-            result = curs.fetchall()
-            if not check(result):
-                print(f"Failed : {sql}")
-                return False
-
-    return True
-
-
-def main():
-    try:
-        dsn = f"dbname='{dbname}' user='{user}' password='{password}' host='{host}' port={port}"
-        with psycopg2.connect(dsn) as conn:
-            result = do_checks(conn)
-            if not result:
-                exit(1)
-    except psycopg2.Error as exc:
-        print("Unable to connect to the database", exc)
-        exit(1)
-
-    print("All checks passed")
-    exit(0)
-
-
 if __name__ == '__main__':
-    main()
+    main(sql_checks)
