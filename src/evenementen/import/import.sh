b618962abdde5c69e92026a782c2aa5c971845cd
Bart Heupers <bart.heupers@gmail.com>
2019-04-01 14:52:28 +0200

    Fix repeated execution for import script for evenementen

--- src/evenementen/import/import.sh
+++ src/evenementen/import/import.sh
@@ -13,7 +13,7 @@ ogr2ogr -f "PGDump" -t_srs EPSG:28992 -s_srs EPSG:4326  -nln evenementen_new ${T
 echo "Create tables & import data for evenementen"
 psql -X --set ON_ERROR_STOP=on <<SQL
 BEGIN;
-DROP TABLE IF EXISTS evenementen_new;
+DROP TABLE IF EXISTS evenementen_new CASCADE;
 COMMIT;
 \i ${TMPDIR}/evenementen.sql
 SQL
@@ -26,8 +26,8 @@ BEGIN;
 ALTER TABLE IF EXISTS evenementen RENAME TO evenementen_old;
 ALTER TABLE evenementen_new RENAME TO evenementen;
 DROP TABLE IF EXISTS evenementen_old CASCADE;
-CREATE UNIQUE INDEX evenementen_idx ON evenementen (ogc_fid);
-CREATE INDEX evenementen_gix ON evenementen USING GIST (wkb_geometry);
+ALTER INDEX evenementen_new_pk RENAME TO evenementen_pk;
+ALTER INDEX evenementen_new_wkb_geometry_geom_idx RENAME TO evenementen_wkb_geometry_geom_idx;
 COMMIT;
 SQL
 
