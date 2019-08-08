56cd9dd867effec68c1dc1e965bf9680f8d51c25
Marten Hoekstra <marten@dolhuis.nl>
2019-08-08 14:02:37 +0200

    Change count to reflect removed record

--- src/milieuzones/import/check_imported_data.py
+++ src/milieuzones/import/check_imported_data.py
@@ -2,7 +2,7 @@
 from utils.check_imported_data import main, all_valid_url, assert_count_minimum, assert_count_zero
 
 sql_checks = [
-    ('count', "select count(*) from milieuzones_new", assert_count_minimum(6)),
+    ('count', "select count(*) from milieuzones_new", assert_count_minimum(5)),
     ('columns', """
 select column_name from information_schema.columns where                                                                  
 table_schema = 'public' and table_name = 'milieuzones_new'
