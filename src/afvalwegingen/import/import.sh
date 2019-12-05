e5f70eeca2209642d731b15644f803a84a952b00
Jan Murre <jan.murre@gmail.com>
2019-12-05 15:48:51 +0100

    More wip.

--- src/afvalwegingen/import/import.sh
+++ src/afvalwegingen/import/import.sh
@@ -30,12 +30,13 @@ psql -X --set ON_ERROR_STOP=on < ${TMPDIR}/wegingen.backup
 echo "Rename tables"
 psql -X --set ON_ERROR_STOP=on <<SQL
 BEGIN;
+-- ALTER TABLE IF EXISTS afvalwegingen RENAME CONSTRAINT pk_wvt_afv_weeggegevens TO pk_wvt_afv_weeggegevens_old ;
 ALTER TABLE IF EXISTS afvalwegingen RENAME TO afvalwegingen_old;
-ALTER TABLE IF EXISTS afvalwegingen RENAME CONSTRAINT pk_wvt_afv_weeggegevens TO pk_wvt_afv_weeggegevens_old ;
 ALTER TABLE his.wvt_afv_weeggegevens SET SCHEMA public;
 ALTER TABLE wvt_afv_weeggegevens RENAME TO afvalwegingen;
 ALTER TABLE afvalwegingen ADD COLUMN combined_pk character varying;
-ALTER TABLE IF EXISTS afvalwegingen_old DROP CONSTRAINT pk_wvt_afv_weeggegevens_old;
+-- ALTER TABLE IF EXISTS afvalwegingen_old DROP CONSTRAINT pk_wvt_afv_weeggegevens_old;
+ALTER TABLE IF EXISTS afvalwegingen RENAME CONSTRAINT pk_wvt_afv_weeggegevens TO pk_afvalwegingen;
 DROP TABLE IF EXISTS afvalwegingen_old;
 -- ALTER INDEX asbest_daken_new_pk RENAME TO asbest_daken_pk;
 ALTER INDEX wvt_afv_weeggegevens_sidx_geo_geometry RENAME TO afvalwegingen_wkb_geometry_geom_idx;
